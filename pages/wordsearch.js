638bee7d7b9a41301dd3f6ae3728288aa59ceb41
CamazingMe <owenbsmith8@gmail.com>
2019-07-26 14:05:38 -0700

    Cells now have adresses that we can write to. "Hello" and "Goodbye" are temporary words we have written to the table.

--- pages/wordsearch.js
+++ pages/wordsearch.js
@@ -1,19 +1,52 @@
 
 $( document ).ready(setup);
 
+var wordGridWidth = 20;
+var wordGridHeight = 20;
+
+var words = [
+    "hello",
+    "goodbye"
+]
+
 function setup() {
-    var wordGridWidth = 20;
-    var wordGridHeight = 20;
+   buildEmptyGrid();
+   fillInWordLetters();
+   fillInRandomLetters();
 
+    
+}
+
+function buildEmptyGrid(){
     for (var y = 0; y < wordGridHeight; y++) {
-        $("table#wordgrid").append("<tr></tr>");
+        $("table#wordgrid").append('<tr id="row' + y + '"></tr>');
         for (var x = 0; x < wordGridWidth;  x++) {
-            var letter = getRandomLetter();
-            $("tr").last().append("<td>" + letter + "</td>");
+            $("tr#row" + y).append('<td id="row' + y +'_col' + x + '"></td>');
         }
+    }  
+}
 
+function fillInWordLetters(){
+    for (var i = 0; i < words.length; i++) {
+        var word = words [i];
+        for (var j = 0; j < word.length; j++) {
+            var letter = word.charAt(j).toUpperCase();
+            $("td#row" + i + "_col" + j).append(letter);
+        }
     }
-    console.log("done with all rows!!!!!!!!!!!!!!!!!!!!!")
+}
+
+function fillInRandomLetters(){
+    for (var y = 0; y < wordGridHeight; y++) {
+        for (var x = 0; x < wordGridWidth;  x++) {
+            var $cell = $("td#row" + y + "_col" + x);
+            var textInCell = $cell.text().trim();
+            if (textInCell == "") {
+                var letter = getRandomLetter();
+                $cell.append(letter);
+            }
+        }
+    }
 }
 
 function getRandomLetter() {
